9fa32afabf93659f50ba4811311fd1b71add2d0f
이현종 <jong2@ihyeonjong-ui-MacBookPro.local>
2023-10-25 03:25:52 +0900

    :memo: Change baseTime 함수 수정(korea타임)

--- utils/getBaseTime.js
+++ utils/getBaseTime.js
@@ -1,7 +1,7 @@
 //! baseDate 구하기 (초단기 실황 baseDate, baseTime 구하는 로직)
 export function getCurrentApiDateAndTime() {
   const now = new Date();
-  const koreaTime = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+  const koreaTime = new Date(now.toLocaleString("en-US", {timeZone: "Asia/Seoul"}));
   const year = koreaTime.getFullYear();
   const month = koreaTime.getMonth() + 1; // 월은 0부터 시작하므로 1을 더함
   let day = koreaTime.getDate();
@@ -28,10 +28,11 @@ export function getCurrentApiDateAndTime() {
   return { baseTime, baseDate };
 }
 
-//! 초단기 api 제공시간은 45분임
+// 초단기 api 제공시간은 45분임
 export function getUrtShortApiDateAndTime() {
   const now = new Date();
-  const koreaTime = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+  const koreaTime = new Date(now.toLocaleString("en-US", {timeZone: "Asia/Seoul"}));
+
   const year = koreaTime.getFullYear();
   const month = koreaTime.getMonth() + 1; // 월은 0부터 시작하므로 1을 더함
   let day = koreaTime.getDate();
@@ -39,19 +40,19 @@ export function getUrtShortApiDateAndTime() {
   let hours = koreaTime.getHours();
   let minutes = koreaTime.getMinutes();
 
-  //만약 0439면 0339으로 base시간이 되어야함 - 40분 주기로 api 제공을 하기 때문에
-  //만약에 0039 여서 2339로 빼야하면 day를 하루 빼어야함
   if (minutes < 45) {
     if (hours === 0) {
       // 자정 이전인 경우, 날짜를 하루 빼야함
       koreaTime.setDate(day - 1);
       day = koreaTime.getDate();
-      hours=23;
+      hours = 23;
     } else {
       hours = hours - 1;
     }
+  } else {
+    minutes = minutes - 45;
   }
-  //만약 지금 시간이 0039라면 날짜 -1 하고 2339가 되어야함
+
   let baseTime = `${hours < 10 ? "0" : ""}${hours}${
     minutes < 10 ? "0" : ""
   }${minutes}`;
@@ -62,11 +63,12 @@ export function getUrtShortApiDateAndTime() {
   return { baseTime, baseDate };
 }
 
+
 //단기예보 api 제공시간은 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
 export function getShortTermApiDateAndTime() {
   const apiTimes = [2310, 2010, 1710, 1410, 1110, 810, 510, 210];
   const now = new Date();
-  const koreaTime = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+  const koreaTime = new Date(now.toLocaleString("en-US", {timeZone: "Asia/Seoul"}));
 
   const currentTime = koreaTime.getHours() * 100 + koreaTime.getMinutes();
 
